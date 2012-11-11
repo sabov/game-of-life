4a311fbb3c6272c839f5a624f6959adab594251c
sabov <alexsabov@gmail.com>
2012-11-11 12:31:59 +0200

    Make tor-like array

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 (function() {
 
 //Universe
-  var Universe = function(col, row) {
-    this.arr  = new Randomizer(col, row);
-    this.grid = new Grid('canvas', col, row);
+  var Universe = function(row, col) {
+    this.arr  = new Randomizer(row, col);
+    this.grid = new Grid('canvas', row, col);
     this.refresh();
-    this.nextStep();
+    this.arr.debug()
+    console.log('Next Step');
+    this.arr = this.nextStep();
+    this.refresh();
+    this.arr.debug()
   }
   Universe.prototype = {
     refresh : function() {
@@ -14,92 +18,79 @@
       }.bind(this));
     },
     nextStep : function() {
-      this.arr.each(function(el, i, j, origArr) {
-        var neighbor = this.arr.getNeighborSumm(i, j);
+      var newStepArr = new TdArray(this.arr.row, this.arr.col);
+      newStepArr.each(function(el, i, j, origArr) {
+        var subArr = this.getSubArr(i, j);
+        el(this.checkNeighbors(subArr));
       }.bind(this));
-    }
-    getNeighborSumm : function(i, j) {
-      if(i < 0 || j < 0 || i > r - 1 || j > c)
-      var result = 0;
-      if(i == 0) {
-        result += a(c, j - 1);
-        result += a(c, j);
-        result += a(c, j + 1);
-      }else {
-        result += a(i - 1, j - 1);
-        result += a(i - 1, j);
-        result += a(i - 1, j + 1);
-      }
-      if(i == r - 1) {
-        result += a(c, j - 1);
-        result += a(c, j);
-        result += a(c, j + 1);
-      }
+      return newStepArr;
+    },
+    checkNeighbors : function(arr) {
+      var count = 0;
+      var result = false;
+      arr.each(function(el, i, j) {
+        if(i == 1 && j == 1) return;
+        if(el()) count++;
+      });
+      if(count > 1 && count < 4) result = true;
+      return result;
     },
     getSubArr : function(i, j) {
-      var a = this.arr;
-      var c = a.col;
-      var r = a.row;
       var subArr = new TdArray(3, 3);
-      if(i > 0 && j > 0 && i < c - 1 && j < r - 1) {
-        subArr.each(function(el, subI, subJ) {
-          var value = a(i - 1 + subI, j - 1 + subJ);
-          el(value);
-        })
-      } else if(i == 0 && j == 0) {
-        var origSubArr = subArr.getArray();
-
-      }
+      subArr.each(function(el, subI, subJ) {
+        var value = this.arr(i - 1 + subI, j - 1 + subJ);
+        el(value);
+      }.bind(this))
+      return subArr;
     }
-
-
   }
 
 //Helper for canvas
-  var Grid = function(canvas, col, row) {
+  var Grid = function(canvas, row, col) {
     this.canvas = document.getElementById(canvas);
     this.ctx    = this.canvas.getContext("2d");
-    this.setSize(col, row);
+    this.setSize(row, col);
     this.refresh();
   }
   Grid.prototype = {
-    setSize : function(col, row) {
-      this.col = col;
+    setSize : function(row, col) {
       this.row = row;
+      this.col = col;
     },
     refresh : function() {
-      this.width  = this.canvas.width = innerWidth;
+      this.width  = this.canvas.width  = innerWidth;
       this.height = this.canvas.height = innerHeight;
       this.colWidth  = Math.round(this.width/this.col);
       this.rowHeight = Math.round(this.height/this.row);
-      this.drawGrid(this.col, this.row);
+      this.drawGrid(this.row, this.col);
     },
-    drawGrid : function(col, row) {
-
+    drawGrid : function(row, col) {
       this.ctx.lineWidth = 0.2;
       this.ctx.strokeStyle = "black";
+      var gridWidth  = col * this.colWidth;
+      var gridHeight = row * this.rowHeight;
 
-      for (var px = 0; px <= this.width; px += this.colWidth) {
+      for (var px = 0; px <= gridWidth; px += this.colWidth) {
         this.ctx.moveTo(0.5 + px, 0);
-        this.ctx.lineTo(0.5 + px, this.height);
+        this.ctx.lineTo(0.5 + px, gridHeight);
       }
-      for (var px = 0; px <= this.height; px += this.rowHeight) {
+      for (var px = 0; px <= gridHeight; px += this.rowHeight) {
         this.ctx.moveTo(0, 0.5 + px);
-        this.ctx.lineTo(this.width, 0.5 + px);
+        this.ctx.lineTo(gridWidth, 0.5 + px);
       }
       this.ctx.stroke();
     },
-    drawCell : function(col, row) {
-      var x1 = col * this.colWidth;
+    drawCell : function(row, col) {
       var y1 = row * this.rowHeight;
+      var x1 = col * this.colWidth;
       this.ctx.fillStyle="#999999";
       this.ctx.fillRect(x1, y1, this.colWidth, this.rowHeight);
     }
   }
 
 //Geting random input for universe
-  var Randomizer = function(col, row) {
-    var arr = new TdArray(col, row);
+  var Randomizer = function(row, col) {
+    var arr = new TdArray(row, col);
     arr.each(function(el, i, j, arr) {
       var r = Math.floor(Math.random()*7);
       el(r == 0? true : false);
@@ -108,15 +99,19 @@
   }
 
 //two-dimensional array
-  var TdArray = function(col, row) {
-    var arr = new Array(col);
+  var TdArray = function(row, col) {
+    var arr = new Array(row);
     for(var i = arr.length - 1; i >= 0; i--) {
-      arr[i] = new Array(row);
+      arr[i] = new Array(col);
     }
 
     //geting access to value in array like arr(i, j)
-    var fn = function(x, y) {
-      return arr[x][y];
+    var fn = function(i, j) {
+      if(i < 0) i = row + i;
+      if(j < 0) j = col + j;
+      if(i > row - 1) i = i - row;
+      if(j > col - 1) j = j - col;
+      return arr[i][j];
     };
     fn.row = row;
     fn.col = col;
@@ -139,6 +134,11 @@
       return arr;
     };
 
+    fn.debug = function() {
+      arr.forEach(function(el) {
+        console.log(el);
+      });
+    }
     return fn;
   }
 
@@ -158,7 +158,7 @@
   };
 
   bindReady(function(){
-    new Universe(40, 20);
+    new Universe(5, 5);
   });
 
 })();
